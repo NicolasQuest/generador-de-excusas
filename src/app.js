ab7ee412318819d0b67036cc278b792a57181acd
Nico <nicolaspasik1@gmail.com>
2025-02-19 13:19:43 +0100

    Ejercicio completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function(){
+
+  // Arrays de excusas
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car', 'my code'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let a = who[Math.floor(Math.random() * who.length)]; 
+    let b = action[Math.floor(Math.random() * action.length)]; 
+    let c = what[Math.floor(Math.random() * what.length)]; 
+    let d = when[Math.floor(Math.random() * when.length)]; 
+
+    document.getElementById("excuse").innerHTML = (`${a} ${b} ${c} ${d}`)
+
+} 
+
+
+
+  //who[a] + action[b] + what[c] + when[d] //--> ???
+
+
+
