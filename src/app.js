c7aa238f140dfc4e05a253ce9ab1a0be0e0b03d8
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-03-31 19:52:47 +0200

    Update app.js to functions

--- src/app.js
+++ src/app.js
@@ -6,16 +6,18 @@ window.onload = function(){
   let what = ['my homework', 'my phone', 'the car', 'my code'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+    function randomizer (arr) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    }
+    
+    let whoRandom = randomizer(who); 
+    let actionRandom = randomizer(action); 
+    let whatRandom = randomizer(what); 
+    let whenRandom = randomizer(when); 
 
-
-    let a = who[Math.floor(Math.random() * who.length)]; 
-    let b = action[Math.floor(Math.random() * action.length)]; 
-    let c = what[Math.floor(Math.random() * what.length)]; 
-    let d = when[Math.floor(Math.random() * when.length)]; 
-
-    document.getElementById("excuse").innerHTML = (`${a} ${b} ${c} ${d}`)
-
+    document.getElementById("excuse").innerHTML = (`${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`)
 } 
+
 
 
 
